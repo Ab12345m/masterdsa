3450585a74b6313d8e0f5799cf7111683449a692
Ab12345m <amilpureabhishek012@gmail.com>
2026-06-19 21:29:43 +0530

    save

--- Array/first.java
+++ Array/first.java
@@ -1,7 +1,21 @@
 package masterdsa.Array;
 
+class Solution {
+    public void pattern5(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        
+    }
+}
+
 public class first {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Solution s = new Solution();
+        s.pattern5(5);
+        
     }
 }
